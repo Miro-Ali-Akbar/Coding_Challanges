60359ea6f4b490cc4b7f5c946f8050ccc276f305
Miro Ali Akbar <Miroaliakbar@gmail.com>
2023-01-16 17:16:36 +0100

    Initial commit

---
+++ Meritvärde.py
@@ -0,0 +1,52 @@
+# 2021-01-16
+def program(linje):
+	points = 0
+	hours = 0
+	if linje == "a":
+		for kurs in natur:
+			points = points + betyg[str(input("Vad fick du för betyg i " + kurs + "?: "))] * natur[kurs]
+			hours = hours + natur[kurs]
+	print(points/hours + float(input("hur mycket bonuspoäng fick du? \n")))
+
+betyg = {
+	"A": 20,
+	"a": 20,
+	"B": 17.5,
+	"b": 17.5,
+	"C": 15,
+	"c": 15,
+	"D": 12.5,
+	"d": 12.5,
+	"E": 10,
+	"e": 10,
+	"F": 0,
+	"f": 0
+}
+
+natur = {
+	"Matte 1": 100,
+	"Matte 2": 100,
+	"Matte 3": 100,
+	"Matte 4": 100,
+	"Kemi 1": 100,
+	"kemi 2": 100,
+	"fysik 1": 150,
+	"fysik 2": 100,
+	"Idrott 1": 100,
+	"Svenska 1": 100,
+	"Svenska 2": 100,
+	"Svenska 3": 100,
+	"Engelska 5": 100,
+	"Engelska 6": 100,
+	"Historia 1": 100,
+	"Samhällskunskap 1": 100,
+	"Religion 1": 50,
+	"Biologi 1": 100,
+	"Biologi 2": 100,
+	"Valfri kurs 1": 100,
+	"Valfri kurs 2": 100,
+	"Valfri kurs 3": 100,
+	"Valfri kurs 4": 100
+}
+
+program(input("Vilken linje gå du på?\n a - Natur \n b - sam \n c - något annat \n"))
